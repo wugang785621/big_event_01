4079cfade0f21d681f172e44a12e48b2fca7f4fa
wugang <969204221@qq.com>
2020-10-27 18:32:39 +0800

    完成主页建设

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,7 +2,15 @@
 var baseUrl = 'http://ajax.frontend.itheima.net'
 
 // 拦截所有ajax请求: get/post/ajax
-$.ajaxPrefilter(function(params) {
+$.ajaxPrefilter(function(options) {
     // 拼接对应环境服务器地址
-    params.url = baseUrl + params.url
+    options.url = baseUrl + options.url
+
+
+    // 统一为有权限的接口,设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+$(function() {
+    // 1.获取用户信息,渲染头像和用户名
+    getUserInfo()
+});
+
+// 获取用户信息必须封装成全局函数(写到入口函数之外)
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        //请求头配置对象
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res) {
+            // console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败!')
+            }
+            // 渲染用户头像
+            renderAvatar(res.data)
+        }
+    })
+}
+
+// 封装用户头像渲染函数
+function renderAvatar(user) {
+    // 1.用户名
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+
+    // 2.用户头像
+    if (user.user_pic !== null) {
+        // 有头像
+        $('.layui-nav-img').show().attr('src', user.user_pic)
+        $('.text-avatar').hide()
+    } else {
+        // 没有头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').show().html(first)
+    }
+}
